4afbd65e692f827103de1b64464a9d5099bda391
vince's servino <vince's servino>
2016-09-16 13:53:20 +0200

    création des vues développeur

--- www/js/app.js
+++ www/js/app.js
@@ -66,6 +66,30 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova','starter.
           }
         }
       })
+      .state('tabs.anthony', {
+        url: "/anthony",
+        views: {
+          'info-tab': {
+            templateUrl: "templates/anthony.html"
+          }
+        }
+      })
+      .state('tabs.clement', {
+        url: "/clement",
+        views: {
+          'info-tab': {
+            templateUrl: "templates/clement.html"
+          }
+        }
+      })
+      .state('tabs.brudel', {
+        url: "/brudel",
+        views: {
+          'info-tab': {
+            templateUrl: "templates/brudel.html"
+          }
+        }
+      })
       .state('tabs.propos', {
         url: "/propos",
         views: {
